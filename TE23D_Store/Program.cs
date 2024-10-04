b5b0b367ba072aef68aad6f4de067ebed7f344b8
Anton Svedin <anton.svedin@elev.ga.ntig.se>
2024-10-04 10:55:59 +0200

    first commit

---
+++ TE23D_Store/Program.cs
@@ -0,0 +1,31 @@
+﻿int i = 0;
+while ( i < 10)
+{
+    Console.WriteLine("Hej");
+    i++;
+}
+
+for (int j = 0; j < 10; j ++)
+{
+    Console.WriteLine("Hej");
+}
+Console.ReadLine();
+
+/* Console.WriteLine("Skriv in din ålder");
+string age = Console.ReadLine();
+
+int ageNUM = 0;
+
+bool success = int.TryParse(age, out ageNUM);
+
+if (success == false)
+{
+    Console.WriteLine("No, a number, idiot!");
+}
+
+if (ageNUM > 18)
+{
+Console.WriteLine("Welcome!");
+}
+
+Console.ReadLine(); */
