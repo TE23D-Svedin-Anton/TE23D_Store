afa5343ed61bfda312955239f0d1e2c487c5893c
Anton Svedin <anton.svedin@elev.ga.ntig.se>
2024-10-04 10:56:19 +0200

    Har börjat med att ge spelarens pengar

--- TE23D_Store/Program.cs
+++ TE23D_Store/Program.cs
@@ -1,31 +1,9 @@
-﻿int i = 0;
-while ( i < 10)
-{
-    Console.WriteLine("Hej");
-    i++;
-}
+﻿int DinaStålar = 100;
 
-for (int j = 0; j < 10; j ++)
+while (DinaStålar > 0)
 {
-    Console.WriteLine("Hej");
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine();
 }
 Console.ReadLine();
-
-/* Console.WriteLine("Skriv in din ålder");
-string age = Console.ReadLine();
-
-int ageNUM = 0;
-
-bool success = int.TryParse(age, out ageNUM);
-
-if (success == false)
-{
-    Console.WriteLine("No, a number, idiot!");
-}
-
-if (ageNUM > 18)
-{
-Console.WriteLine("Welcome!");
-}
-
-Console.ReadLine(); */
